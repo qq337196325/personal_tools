921916f2f27d4df6e1f9d18a1c098b2f868904f9
zouyougui <qq337196325@gmail.com>
2018-11-22 14:30:48 +0800

    测试

---
+++ src/miscellaneous.php
@@ -0,0 +1,27 @@
+<?php
+namespace public_api;
+
+class miscellaneous {
+    
+    public function test(){
+        return '12345';
+    }
+
+    
+
+    public function post($url, $post_json) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $ret = curl_exec($ch);
+
+    curl_close($ch);
+    return $ret;
+  }
+
+}
